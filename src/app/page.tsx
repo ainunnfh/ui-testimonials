88ca136b170a6ff79d805f0218a9e8ddc0b5c669
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-09-02 15:38:17 +0700

    💄update (page): create arrow button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import TestimonialCard from "@/components/TestimonialCard";
 import Image from "next/image";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
+
+import { IoIosArrowDropleft } from "react-icons/io";
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between p-10">
-      <div className=" flex flex-col items-center  text-2xl font-bold">
-        What Our Costumers Are Saying
+    <div className="flex flex-col p-10">
+      <div className="flex justify-between">
+        <div className=" flex flex-col text-2xl font-bold justify-between">
+          What Our Costumers
+          <span>Are Saying</span>
+        </div>
+        <div className="flex text-4xl items-center">
+          <button>
+            <IoIosArrowDropleft />
+          </button>
+          <button>
+            <IoIosArrowDroprightCircle />
+          </button>
+        </div>
       </div>
 
       <div className=" grid grid-cols-3 gap-3 justify-center mt-8">
