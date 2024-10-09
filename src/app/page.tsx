94f763b635cbd7bfc572e77bd61f6b38c16a1c0a
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-10-09 22:37:43 +0700

    💩try to use slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,17 @@ import {
   IoIosArrowDropleftCircle,
   IoIosArrowDroprightCircle,
 } from "react-icons/io";
+import Slider from "react-slick";
 
 import { IoIosArrowDropleft } from "react-icons/io";
 export default function Home() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className="flex flex-col p-10 min-h-screen justify-center">
       <div className="flex justify-between">
@@ -23,12 +31,14 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="flex flex-row gap-3 justify-around overflow-x-scroll">
+      <div className="flex flex-row gap-3 justify-around ">
+        {/* <Slider {...settings}> */}
         <TestimonialCard />
         <TestimonialCard />
         <TestimonialCard />
         <TestimonialCard />
         <TestimonialCard />
+        {/* </Slider> */}
       </div>
     </div>
   );
